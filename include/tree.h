2510f64c989eb3acf99e91eec2ba8584259f0412
malishev-danil <malishev-danil5@yandex.ru>
2025-06-07 22:04:33 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -2,4 +2,32 @@
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
 
+#include <vector>
+#include <memory>
+
+class PMTreeNode {
+public:
+    char value;
+    std::vector<std::shared_ptr<PMTreeNode>> children;
+
+    PMTreeNode(char val) : value(val) {}
+};
+
+class PMTree {
+private:
+    std::shared_ptr<PMTreeNode> root;
+    std::vector<char> symbols;
+
+    void buildPermutationTree(const std::vector<char>& remaining, std::shared_ptr<PMTreeNode> node);
+
+public:
+    PMTree(const std::vector<char>& symbols);
+    std::shared_ptr<PMTreeNode> getRoot() const { return root; }
+    const std::vector<char>& getSymbols() const { return symbols; }
+
+    friend std::vector<std::vector<char>> getAllPerms(PMTree& tree);
+    friend std::vector<char> getPerm1(PMTree& tree, int num);
+    friend std::vector<char> getPerm2(PMTree& tree, int num);
+};
+
 #endif  // INCLUDE_TREE_H_
