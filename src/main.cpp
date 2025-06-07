77f26d75c5e39919affb78014c82058c54d645a0
malishev-danil <malishev-danil5@yandex.ru>
2025-06-07 22:59:47 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -34,11 +34,8 @@ int main() {
         }
         std::cout << "\n";
     }
-
     std::cout << "\nTime comparison test:\n";
-
-    PMTree bigTree({'1','2','3','4','5','6','7','8'});
-
+    PMTree bigTree({'1', '2', '3', '4', '5', '6', '7', '8'});
     auto start = std::chrono::high_resolution_clock::now();
     getPerm1(bigTree, 20000);
     auto end = std::chrono::high_resolution_clock::now();
